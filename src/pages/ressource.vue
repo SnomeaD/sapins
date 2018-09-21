d47df0e2b450c1092a8c1820d75f2c869551c3e9
SnomeaD <snomead@gmail.com>
2018-09-21 11:24:50 +0200

    Change ressource layout

--- src/pages/ressource.vue
+++ src/pages/ressource.vue
@@ -20,6 +20,8 @@
                     <a href="https://www.wowprogress.com/guild/eu/sargeras/Les+Sapins+de+la+Horde" target="_blank">
                         <img class="img-fluid logo-ressource" src="../img/wowprogress.png">WoWProgress
                     </a><br />
+                </div>
+                <div class="col-sm">
                     <a href="https://raider.io/guilds/eu/sargeras/Les%20Sapins%20de%20la%20Horde" target="_blank">
                         <img class="img-fluid logo-ressource" src="../img/raiderio_square_xl.png">Raider.io
                     </a><br />
@@ -44,6 +46,8 @@
                     <a href="https://www.wowanalyzer.com/" target="_blank">
                         <img class="img-fluid logo-ressource" src="../img/raidbots.jpg">Raidbots
                     </a><br />
+                </div>
+                <div class="col-sm">
                     <a href="https://www.herodamage.com/" target="_blank">
                         <img class="img-fluid logo-ressource" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAyNCIgaGVpZ2h0PSIxMDI0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHBhdGggZD0iTTcyMCA0OTZMMjA1IDkyMCA0NSAzMDRsMTgxIDM2NiAzMDItMzY2LTY0LTY0IDQwOS04OS04OSA0MDktNjQtNjR6IiBmaWxsPSIjQjcxQzFDIi8+PHBhdGggZD0iTTUyNCA1MjRsLTcyIDcyLTI0LTI0IDcyLTcyLTUwLjUtNTAuNUw0NjQgNDMybDMyIDMyIDIyNC0yMjQgODktMjUtMjUgODktMjI0IDIyNCAzMiAzMi0xNy41IDE0LjVMNTI0IDUyNHoiIGZpbGw9IiNGRkYiLz48cGF0aCBkPSJNNDAxLjk0MSA1NzYuOTQxbDU2LjU2OS0xMS4zMTQtMTEuMzE0IDU2LjU2OS01Ni41NjkgMTEuMzE0IDExLjMxNC01Ni41Njl6IiBmaWxsPSIjRkZGIi8+PHBhdGggZD0iTTc0MCAyNjBsMjQgMjQtMjA4IDIwOC0yNC0yNCAyMDgtMjA4em0zMy05bC05IDMzLTI0LTI0IDMzLTl6IiBmaWxsPSIjQjcxQzFDIi8+PC9nPjwvc3ZnPg==">Hero Damage
                     </a><br />
@@ -65,6 +69,8 @@
                     <a href="https://www.mmo-champion.com/" target="_blank">
                         <img class="img-fluid logo-ressource logo-ressource-large" src="../img/mmo-champion.png">MMO Champion
                     </a><br />
+                </div>
+                <div class="col-sm">
                     <a href="https://worldofwarcraft.judgehype.com/" target="_blank">
                         <img class="img-fluid logo-ressource logo-ressource-large" src="../img/judgehype.png">JudgeHype
                     </a><br />
