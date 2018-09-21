2f740e6d67de876d5f93e29d234cc06f3f446a6f
SnomeaD <snomead@gmail.com>
2018-09-21 10:32:05 +0200

    Add new ressources

--- src/pages/ressource.vue
+++ src/pages/ressource.vue
@@ -23,6 +23,9 @@
                     <a href="https://raider.io/guilds/eu/sargeras/Les%20Sapins%20de%20la%20Horde" target="_blank">
                         <img class="img-fluid logo-ressource" src="../img/raiderio_square_xl.png">Raider.io
                     </a><br />
+                    <a href="https://wowaudit.com/eu/sargeras/les-sapins-de-la-horde/main" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/spreadsheet.png">WoW Audit Spreadsheet
+                    </a><br />
                 </div>
             </div>   
             <div class="row">
@@ -41,6 +44,12 @@
                     <a href="https://www.wowanalyzer.com/" target="_blank">
                         <img class="img-fluid logo-ressource" src="../img/raidbots.jpg">Raidbots
                     </a><br />
+                    <a href="https://www.herodamage.com/" target="_blank">
+                        <img class="img-fluid logo-ressource" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAyNCIgaGVpZ2h0PSIxMDI0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PHBhdGggZD0iTTcyMCA0OTZMMjA1IDkyMCA0NSAzMDRsMTgxIDM2NiAzMDItMzY2LTY0LTY0IDQwOS04OS04OSA0MDktNjQtNjR6IiBmaWxsPSIjQjcxQzFDIi8+PHBhdGggZD0iTTUyNCA1MjRsLTcyIDcyLTI0LTI0IDcyLTcyLTUwLjUtNTAuNUw0NjQgNDMybDMyIDMyIDIyNC0yMjQgODktMjUtMjUgODktMjI0IDIyNCAzMiAzMi0xNy41IDE0LjVMNTI0IDUyNHoiIGZpbGw9IiNGRkYiLz48cGF0aCBkPSJNNDAxLjk0MSA1NzYuOTQxbDU2LjU2OS0xMS4zMTQtMTEuMzE0IDU2LjU2OS01Ni41NjkgMTEuMzE0IDExLjMxNC01Ni41Njl6IiBmaWxsPSIjRkZGIi8+PHBhdGggZD0iTTc0MCAyNjBsMjQgMjQtMjA4IDIwOC0yNC0yNCAyMDgtMjA4em0zMy05bC05IDMzLTI0LTI0IDMzLTl6IiBmaWxsPSIjQjcxQzFDIi8+PC9nPjwvc3ZnPg==">Hero Damage
+                    </a><br />
+                    <a href="https://bloodmallet.com/" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/bloodmallet.svg">bloodmallet
+                    </a><br />
                 </div>
             </div>
              <div class="row">
@@ -58,6 +67,9 @@
                     </a><br />
                     <a href="https://worldofwarcraft.judgehype.com/" target="_blank">
                         <img class="img-fluid logo-ressource logo-ressource-large" src="../img/judgehype.png">JudgeHype
+                    </a><br />
+                    <a href="https://wago.io/" target="_blank">
+                        <img class="img-fluid logo-ressource logo-ressource-large" src="../img/wago.png">wago.io
                     </a><br />
                 </div>
             </div>
