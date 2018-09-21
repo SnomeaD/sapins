fd87b2bd828267922270e3c1bd1a57dc03287745
SnomeaD <snomead@gmail.com>
2018-09-21 10:17:19 +0200

    Add missing file

---
+++ src/pages/ressource.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container">
+         <div id="title" class="container-fluid">
+            <div class="text-center"><h1>Les Ressources</h1></div>
+        </div>
+        <div id="content" class="container">
+            <div class="row">
+                <div class="col-sm text-center">
+                    <h1>Les Sapins de la Horde sur internet</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <a href="http://eu.battle.net/wow/en/guild/sargeras/Les_Sapins_de_la_Horde/" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/wow_square_xl.png">L'armurerie battle.net
+                    </a><br />
+                    <a href="https://www.warcraftlogs.com/guild/eu/sargeras/les%20sapins%20de%20la%20horde" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/warcraftlog.png">WarcraftLog
+                    </a><br />
+                    <a href="https://www.wowprogress.com/guild/eu/sargeras/Les+Sapins+de+la+Horde" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/wowprogress.png">WoWProgress
+                    </a><br />
+                    <a href="https://raider.io/guilds/eu/sargeras/Les%20Sapins%20de%20la%20Horde" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/raiderio_square_xl.png">Raider.io
+                    </a><br />
+                </div>
+            </div>   
+            <div class="row">
+                <div class="col-sm text-center">
+                    <h1>Pour être plus fort!</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <a href="https://www.wowanalyzer.com/" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/wowanalyzer.png">WoW­Analyzer
+                    </a><br />
+                    <a href="https://www.wipefest.net/guild/les%20Sapins%20de%20la%20Horde/Sargeras/EU" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/wipefest.png">WIPEFEST
+                    </a><br />
+                    <a href="https://www.wowanalyzer.com/" target="_blank">
+                        <img class="img-fluid logo-ressource" src="../img/raidbots.jpg">Raidbots
+                    </a><br />
+                </div>
+            </div>
+             <div class="row">
+                <div class="col-sm text-center">
+                    <h1>Les sites utiles</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <a href="https://www.wowhead.com/" target="_blank">
+                        <img class="img-fluid logo-ressource logo-ressource-large" src="../img/wowhead.png">Wowhead
+                    </a><br />
+                    <a href="https://www.mmo-champion.com/" target="_blank">
+                        <img class="img-fluid logo-ressource logo-ressource-large" src="../img/mmo-champion.png">MMO Champion
+                    </a><br />
+                    <a href="https://worldofwarcraft.judgehype.com/" target="_blank">
+                        <img class="img-fluid logo-ressource logo-ressource-large" src="../img/judgehype.png">JudgeHype
+                    </a><br />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
